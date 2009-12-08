6be0e71ac808717bbb3656ed39db5be0c95a013f
Gary King <gking@nvidia.com>
2009-12-07 20:52:35 -0800

    usb: host: ehci-hcd: add controller_resets_phy quirk
    only reset the controller when doing so won't also reset the phy (Tegra quirk)
    
    Change-Id: I549a18977d0d5ebfa12c32016aa9e6bffaa8643c
    Signed-off-by: Gary King <gking@nvidia.com>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -134,6 +134,7 @@ struct ehci_hcd {			/* one per controller */
 	unsigned		amd_l1_fix:1;
 	unsigned		fs_i_thresh:1;	/* Intel iso scheduling */
 	unsigned		use_dummy_qh:1;	/* AMD Frame List table quirk*/
+	unsigned		controller_resets_phy:1;
 
 	/* required for usb32 quirk */
 	#define OHCI_CTRL_HCFS          (3 << 6)
