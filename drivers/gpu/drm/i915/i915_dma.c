2734b2ab50eee0af7d985f4da2ffe0faafadf791
Bryan Freed <bfreed@google.com>
2010-06-22 15:59:43 -0700

    CHROMIUM: Implement direct pineview backlight control.
    This adds a Kconfig variable CONFIG_DRM_I915_DIRECT_BACKLIGHT to
    optionally include the new source file i915_backlight.c.  This file
    registers a couple backlight set/get routines with the standard
    drivers/video/backlight interface to add a directory under
    /sys/class/backlight/.
    
    The big problem I see with this modification is that the i915 driver
    _already_ supports backlight level adjustment in i915_opregion.c.
    Unfortunately, that requires an IRQ to occur as the result of writing
    some ASLE (ACPI Source Language Event) register, and this requires
    BIOS support for the ACPI hook.  It looks like we will not have such
    support in our initial custom BIOS.
    
    So I copy a small amount of code from i915_opregion.c to make backlight
    adjustment directly available to the backlight framework.
    
    Another problem is that I do not cover all the cases handled by the
    i915_opregion.c code.  It would be too much untested code copying to
    do so.  I enforce the lack of support by checking for IS_PINEVIEW()
    in i915_backlight_init().
    
    Review URL: http://codereview.chromium.org/2830015
    
    Change-Id: I73b77996e229c1e74a019d5929ddd972537492ed
    Reviewed-on: http://gerrit.chromium.org/gerrit/9975
    Reviewed-by: Bryan Freed <bfreed@chromium.org>
    Tested-by: Simon Que <sque@chromium.org>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -2066,6 +2066,7 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	i915_mch_dev = dev_priv;
 	dev_priv->mchdev_lock = &mchdev_lock;
 	spin_unlock(&mchdev_lock);
+	i915_backlight_init(dev);
 
 	ips_ping_for_i915_load();
 
@@ -2105,6 +2106,7 @@ int i915_driver_unload(struct drm_device *dev)
 	spin_lock(&mchdev_lock);
 	i915_mch_dev = NULL;
 	spin_unlock(&mchdev_lock);
+	i915_backlight_exit(dev);
 
 	if (dev_priv->mm.inactive_shrinker.shrink)
 		unregister_shrinker(&dev_priv->mm.inactive_shrinker);
--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -37,6 +37,10 @@
 #include <linux/i2c.h>
 #include <drm/intel-gtt.h>
 
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+#include <linux/backlight.h>
+#endif
+
 /* General customization:
  */
 
@@ -723,6 +727,11 @@ typedef struct drm_i915_private {
 	struct drm_property *force_audio_property;
 
 	atomic_t forcewake_count;
+
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+	/* direct backlight interface */
+	struct backlight_device *backlight;
+#endif
 } drm_i915_private_t;
 
 enum i915_cache_level {
@@ -1277,6 +1286,15 @@ extern void intel_unregister_dsm_handler(void);
 static inline void intel_register_dsm_handler(void) { return; }
 static inline void intel_unregister_dsm_handler(void) { return; }
 #endif /* CONFIG_ACPI */
+
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+/* i915_backlight.c */
+extern void i915_backlight_init(struct drm_device *dev);
+extern void i915_backlight_exit(struct drm_device *dev);
+#else
+extern inline void i915_backlight_init(struct drm_device *dev) { return; }
+extern inline void i915_backlight_exit(struct drm_device *dev) { return; }
+#endif
 
 /* modesetting */
 extern void intel_modeset_init(struct drm_device *dev);
