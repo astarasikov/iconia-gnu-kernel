0e6f6c6c20ab4c260b99cc3a65c0f05fea46834d
Gary King <gking@nvidia.com>
2010-08-03 14:54:46 -0700

    tegra: ventana: use tegra_gart_device rather than local definition
    Change-Id: I6cb57dd46162c258e32c4946de5a649daff3a79b
    Signed-off-by: Gary King <gking@nvidia.com>

--- arch/arm/mach-tegra/board-ventana.c
+++ arch/arm/mach-tegra/board-ventana.c
@@ -90,28 +90,6 @@ static struct platform_device pda_power_device = {
 	},
 };
 
-static struct resource tegra_gart_resources[] = {
-	{
-		.name = "mc",
-		.flags = IORESOURCE_MEM,
-		.start = TEGRA_MC_BASE,
-		.end = TEGRA_MC_BASE + TEGRA_MC_SIZE - 1,
-	},
-	{
-		.name = "gart",
-		.flags = IORESOURCE_MEM,
-		.start = 0x58000000,
-		.end = 0x58000000 - 1 + 32 * 1024 * 1024,
-	}
-};
-
-static struct platform_device tegra_gart_dev = {
-	.name = "tegra_gart",
-	.id = -1,
-	.num_resources = ARRAY_SIZE(tegra_gart_resources),
-	.resource = tegra_gart_resources
-};
-
 static struct tegra_sdhci_platform_data sdhci_pdata1 = {
 	.cd_gpio	= -1,
 	.wp_gpio	= -1,
@@ -187,7 +165,7 @@ static struct platform_device *ventana_devices[] __initdata = {
 	&tegra_sdhci_device3,
 	&tegra_sdhci_device4,
 	&pda_power_device,
-	&tegra_gart_dev,
+	&tegra_gart_device,
 };
 
 static int ventana_touch_reset(void)
