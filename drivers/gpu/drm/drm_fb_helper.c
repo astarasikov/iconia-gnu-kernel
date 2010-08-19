17784573e0aacd1fa095a082f144a30740d2ae4c
Hugh Dickins <hughd@chromium.org>
2010-08-19 13:10:03 -0700

    3/8 CHROMIUM: panic=-1 for immediate reboot
    When kernel BUG or oops occurs, ChromeOS intends to panic and immediately
    reboot, with stacktrace and other messages preserved in RAM across reboot.
    But the longer we delay, the more likely the user is to poweroff and lose
    the info.
    
    panic_timeout (seconds before rebooting) is set by panic= boot option
    or sysctl or /proc/sys/kernel/panic; but 0 means wait forever, so at
    present we have to delay at least 1 second.
    
    Let a negative number mean reboot immediately (with the small cosmetic
    benefit of suppressing that newline-less "Rebooting in %d seconds.."
    message).
    
    And check for it in the drm_fb_helper_panic() notifier: if we're going
    to reboot immediately, the user will not be able to see the messages
    anyway, and messing with the video mode may display artifacts, and
    certainly get into several layers of complexity (including mutexes and
    memory allocations) which we shall be much safer to avoid.
    
    (Unrelated minor change here, copied from Jesse Barnes: drm_fb_helper_panic() had two consecutive return statements;
    since the return value from drm_fb_helper_force_kernel_mode is of no interest to panic(), choose simply to return 0.)
    
    BUG=chromium-os:2269
    TEST=manual
    
    Signed-off-by: Hugh Dickins <hughd@chromium.org>
    
    Review URL: http://codereview.chromium.org/3067030
    
    Change-Id: I476f7f0742c7a0aa9f55be682478e050eab29395

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -371,8 +371,14 @@ bool drm_fb_helper_force_kernel_mode(void)
 int drm_fb_helper_panic(struct notifier_block *n, unsigned long ununsed,
 			void *panic_str)
 {
-	printk(KERN_ERR "panic occurred, switching back to text console\n");
-	return drm_fb_helper_force_kernel_mode();
+	/*
+	 * It's a waste of time and effort to switch back to text console
+	 * if the kernel should reboot before panic messages can be seen.
+	 */
+	if (panic_timeout >= 0) {
+		DRM_ERROR("panic occurred, switching back to text console\n");
+		drm_fb_helper_force_kernel_mode();
+	}
 	return 0;
 }
 EXPORT_SYMBOL(drm_fb_helper_panic);
