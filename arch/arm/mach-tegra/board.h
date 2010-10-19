cd0e81459545f6097122b78701baab8abe1ceeec
Colin Cross <ccross@android.com>
2010-10-19 16:12:33 -0700

    tegra: Add function to enable protected aperture
    Change-Id: I699cee3bc9a131259b330655126146f5d1f92043
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -32,6 +32,7 @@ void __init tegra_init_clock(void);
 int __init tegra_pcie_init(bool init_port0, bool init_port1);
 void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 	unsigned long fb2_size);
+void __init tegra_protected_aperture_init(unsigned long aperture);
 
 extern unsigned long tegra_bootloader_fb_start;
 extern unsigned long tegra_bootloader_fb_size;
@@ -43,6 +44,7 @@ extern unsigned long tegra_carveout_start;
 extern unsigned long tegra_carveout_size;
 extern unsigned long tegra_lp0_vec_start;
 extern unsigned long tegra_lp0_vec_size;
+extern unsigned long tegra_grhost_aperture;
 
 extern struct sys_timer tegra_timer;
 #endif
