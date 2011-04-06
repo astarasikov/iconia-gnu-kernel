c58d5abc7f82ee1aba0d78ccefdfdd50611f4a09
Robert Morell <rmorell@nvidia.com>
2011-04-05 19:43:35 -0700

    CHROMIUM: video: tegra: Allow fractional input rects
    This change makes the input rect for Tegra windows be a 20.12
    fixed-point number instead of an integer.  This allows software to
    specify sub-pixel precision.
    
    BUG=chrome-os-partner:1812
    TEST=After the patch series, ran X server on Seaboard, verified HDMI hotplug works with xrandr
    Signed-off-by: Robert Morell <rmorell@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/6716009
    
    Change-Id: Ifc72db14f7e5c81dfb2831dd55e4cfa235b06e3c

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -20,6 +20,7 @@
 #ifndef __MACH_TEGRA_DC_H
 #define __MACH_TEGRA_DC_H
 
+#include <drm/drm_fixed.h>
 
 #define TEGRA_MAX_DC		2
 #define DC_N_WINDOWS		3
@@ -100,10 +101,10 @@ struct tegra_dc_win {
 	unsigned		offset_v;
 	unsigned		stride;
 	unsigned		stride_uv;
-	unsigned		x;
-	unsigned		y;
-	unsigned		w;
-	unsigned		h;
+	fixed20_12		x;
+	fixed20_12		y;
+	fixed20_12		w;
+	fixed20_12		h;
 	unsigned		out_x;
 	unsigned		out_y;
 	unsigned		out_w;
