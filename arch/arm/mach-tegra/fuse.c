RonGokhale/codeaurora_kernel

CHROMIUM: tegra: only read sku_id once
This also resolves a panic-on-resume when the fuse block is touched too soon after resume.

Change-Id: I18a36bb43957fa03daba9ee4ed2dbefb2eb97a5d
Signed-off-by: Olof Johansson <olofj@chromium.org>

BUG=None
TEST=After removing brcmfmac due to driver bugs, try echo mem > /sys/power/state and resume

Review URL: http://codereview.chromium.org/6409037

arch/arm/mach-tegra/fuse.c
@@ -37,6 +37,8 @@
 
 static DEFINE_MUTEX(tegra_fuse_dma_lock);
 
+int tegra_sku_id;
+
 #ifdef CONFIG_TEGRA_SYSTEM_DMA
 static struct tegra_dma_channel *tegra_fuse_dma;
 static u32 *tegra_fuse_bb;
@@ -154,9 +156,11 @@ void tegra_init_fuse(void)
 	reg |= 1 << 28;
 	writel(reg, IO_TO_VIRT(TEGRA_CLK_RESET_BASE + 0x48));
 
+	tegra_sku_id = fuse_readl(FUSE_SKU_INFO) & 0xff;
+
 	pr_info("Tegra Revision: %s SKU: %d CPU Process: %d Core Process: %d\n",
 		tegra_revision_name[tegra_get_revision()],
-		tegra_sku_id(), tegra_cpu_process_id(),
+		tegra_sku_id, tegra_cpu_process_id(),
 		tegra_core_process_id());
 }
 
@@ -188,14 +192,6 @@ unsigned long long tegra_chip_uid(void)
 	lo = fuse_readl(FUSE_UID_LOW);
 	hi = fuse_readl(FUSE_UID_HIGH);
 	return (hi << 32ull) | lo;
-}
-
-int tegra_sku_id(void)
-{
-	int sku_id;
-	u32 reg = fuse_readl(FUSE_SKU_INFO);
-	sku_id = reg & 0xFF;
-	return sku_id;
 }
 
 int tegra_cpu_process_id(void)

astarasikov/iconia-gnu-kernel

arch/arm/mach-tegra/fuse.c
@@ -30,6 +30,8 @@
 #define FUSE_SKU_INFO		0x110
 #define FUSE_SPARE_BIT		0x200
 
+int tegra_sku_id;
+
 static const char *tegra_revision_name[TEGRA_REVISION_MAX] = {
 	[TEGRA_REVISION_UNKNOWN] = "unknown",
 	[TEGRA_REVISION_A02] = "A02",
@@ -58,9 +60,11 @@ void tegra_init_fuse(void)
 	reg |= 1 << 28;
 	writel(reg, IO_TO_VIRT(TEGRA_CLK_RESET_BASE + 0x48));
 
+	tegra_sku_id = tegra_fuse_readl(FUSE_SKU_INFO) & 0xff;
+
 	pr_info("Tegra Revision: %s SKU: %d CPU Process: %d Core Process: %d\n",
 		tegra_revision_name[tegra_get_revision()],
-		tegra_sku_id(), tegra_cpu_process_id(),
+		tegra_sku_id, tegra_cpu_process_id(),
 		tegra_core_process_id());
 }
 
@@ -71,14 +75,6 @@ unsigned long long tegra_chip_uid(void)
 	lo = tegra_fuse_readl(FUSE_UID_LOW);
 	hi = tegra_fuse_readl(FUSE_UID_HIGH);
 	return (hi << 32ull) | lo;
-}
-
-int tegra_sku_id(void)
-{
-	int sku_id;
-	u32 reg = tegra_fuse_readl(FUSE_SKU_INFO);
-	sku_id = reg & 0xFF;
-	return sku_id;
 }
 
 int tegra_cpu_process_id(void)