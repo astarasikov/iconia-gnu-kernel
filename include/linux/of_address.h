505e41a99797a2ef2359b2f3e73439a9b3e30f98
Grant Likely <grant.likely@secretlab.ca>
2011-04-28 22:03:35 -0600

    CHROMIUM: of/address: Add of_find_matching_node_by_address helper
    of_find_matching_node_by_address() can be used to find a device tree
    node for a device at a specific address.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/of_address.h
+++ include/linux/of_address.h
@@ -7,6 +7,10 @@ extern int of_address_count(struct device_node *np);
 extern u64 of_translate_address(struct device_node *np, const __be32 *addr);
 extern int of_address_to_resource(struct device_node *dev, int index,
 				  struct resource *r);
+extern struct device_node *of_find_matching_node_by_address(
+					struct device_node *from,
+					const struct of_device_id *matches,
+					u64 base_address);
 extern void __iomem *of_iomap(struct device_node *device, int index);
 
 /* Extract an address from a device, returns the region size and
