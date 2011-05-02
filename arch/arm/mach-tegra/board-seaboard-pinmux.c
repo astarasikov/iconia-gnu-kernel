372a614052e713738376057bc689ccb6d4013b1d
Rhyland Klein <rklein@nvidia.com>
2011-05-02 15:37:48 -0700

    CHROMIUM: tegra: enable Bluetooth
    This change enables BT on all platforms. By default on boot, BT
    will be disabled (blocked by rfkill). To enable BT, you can use
    the gpios exported into /sys/class/gpio.
    
    After boot, to unblock BT radio:
    echo 1 > /sys/class/gpio/gpio82/value
    echo 1 > /sys/class/gpio/gpio160/value
    
    BUG=chrome-os-partner:1949
    TEST=boot kernel, verify that BT can be enabled by setting
    both GPIO's to 1 to unblock the radio.
    
    Change-Id: Ice3f7a022dfd01f578f80191a49f7a8608a23835
    
    R=olofj@chromium.org
    Signed-off-by: Rhyland Klein <rklein@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/6889009

--- arch/arm/mach-tegra/board-seaboard-pinmux.c
+++ arch/arm/mach-tegra/board-seaboard-pinmux.c
@@ -157,8 +157,8 @@ static __initdata struct tegra_pingroup_config seaboard_pinmux[] = {
 	{TEGRA_PINGROUP_UAB,   TEGRA_MUX_ULPI,          TEGRA_PUPD_PULL_UP,   TEGRA_TRI_NORMAL},
 	{TEGRA_PINGROUP_UAC,   TEGRA_MUX_RSVD2,         TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
 	{TEGRA_PINGROUP_UAD,   TEGRA_MUX_IRDA,          TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
-	{TEGRA_PINGROUP_UCA,   TEGRA_MUX_UARTC,         TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
-	{TEGRA_PINGROUP_UCB,   TEGRA_MUX_UARTC,         TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
+	{TEGRA_PINGROUP_UCA,   TEGRA_MUX_UARTC,         TEGRA_PUPD_PULL_UP,   TEGRA_TRI_NORMAL},
+	{TEGRA_PINGROUP_UCB,   TEGRA_MUX_UARTC,         TEGRA_PUPD_PULL_UP,   TEGRA_TRI_NORMAL},
 	{TEGRA_PINGROUP_UDA,   TEGRA_MUX_ULPI,          TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
 	{TEGRA_PINGROUP_CK32,  TEGRA_MUX_NONE,          TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
 	{TEGRA_PINGROUP_DDRC,  TEGRA_MUX_NONE,          TEGRA_PUPD_NORMAL,    TEGRA_TRI_NORMAL},
@@ -196,6 +196,8 @@ static struct tegra_gpio_table gpio_table[] = {
 	{ .gpio = TEGRA_GPIO_HDMI_ENB,		.enable = true },
 	{ .gpio = TEGRA_GPIO_MPU3050_IRQ,	.enable = true },
 	{ .gpio = TEGRA_GPIO_DISABLE_CHARGER,	.enable = true },
+	{ .gpio = TEGRA_GPIO_BT_RESET,		.enable = true },
+	{ .gpio = TEGRA_GPIO_BT_SHUTDOWN,	.enable = true },
 };
 
 void __init seaboard_pinmux_init(void)
@@ -223,4 +225,17 @@ void __init seaboard_pinmux_init(void)
 					ARRAY_SIZE(seaboard_drive_pinmux));
 
 	tegra_gpio_config(gpio_table, ARRAY_SIZE(gpio_table));
+
+	/* exporting BT gpio's until the rfkill driver
+	 * is availabe upstream. Setting both gpio's to
+	 * 0, which blocks the BT radio. Set to 1 to enable.
+	 * This can be done from /sys/class/gpio
+	 */
+	gpio_request(TEGRA_GPIO_BT_RESET, "bt_nreset_gpio");
+	gpio_direction_output(TEGRA_GPIO_BT_RESET, 0);
+	gpio_export(TEGRA_GPIO_BT_RESET, false);
+
+	gpio_request(TEGRA_GPIO_BT_SHUTDOWN, "bt_nshutdown_gpio");
+	gpio_direction_output(TEGRA_GPIO_BT_SHUTDOWN, 0);
+	gpio_export(TEGRA_GPIO_BT_SHUTDOWN, false);
 }
