9a3d43a36ad8291041d42dbad70c38bce724aede
Jason Glasgow <jglasgow@chromium.org>
2011-05-13 10:02:57 -0400

    CHROMIUM: Add Sony Gobi 3000 vid:pid.
    Note that the QDL vid:pid is not yet tested, since I don't have a physical
    sample.
    
    BUG=chrome-os-partner:3778
    TEST=Adhoc
    Built. No hardware yet, so no test yet.
    
    Change-Id: Ibf130fd6abd0add77ca214daac27955570ead58a
    Signed-off-by: Jason Glasgow <jglasgow@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/846
    Reviewed-by: Elly Jones <ellyjones@chromium.org>

--- drivers/usb/serial/qcserial.c
+++ drivers/usb/serial/qcserial.c
@@ -103,6 +103,8 @@ static const struct usb_device_id id_table[] = {
 	{USB_DEVICE_DRVINFO(0x1410, 0xa021, GOBI3K)},	/* Novatel Gobi 3000 Composite */
 	{USB_DEVICE_DRVINFO(0x413c, 0x8193, GOBI3K)},	/* Dell Gobi 3000 QDL */
 	{USB_DEVICE_DRVINFO(0x413c, 0x8194, GOBI3K)},	/* Dell Gobi 3000 Composite */
+	{USB_DEVICE_DRVINFO(0x12D1, 0x14F0, GOBI3K)},	/* Sony Gobi 3000 QDL */
+	{USB_DEVICE_DRVINFO(0x12D1, 0x14F1, GOBI3K)},	/* Sony Gobi 3000 Composite */
 	{ }				/* Terminating entry */
 };
 MODULE_DEVICE_TABLE(usb, id_table);
