a35191d4a051028e9930133b06b65a89c8e64dcd
Sameer Nanda <snanda@chromium.org>
2011-06-09 16:58:10 -0700

    CHROMIUM: tegra: enable LP0 state
    Re-enable LP0 state for suspend-to-RAM.
    
    BUG=chrome-os-partner:3388
    TEST=cat /sys/kernel/debug/suspend_mode and ensure that it is set to lp0.
    Then do a suspend/resume cycle and make sure that the system resumes
    properly.
    
    Change-Id: I5e263a3857fd0cb486bc8af5bb2aa5b1f125c45a
    Signed-off-by: Sameer Nanda <snanda@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/2448

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -710,7 +710,7 @@ static struct tegra_suspend_platform_data seaboard_suspend = {
 	.core_off_timer = 0x7f,
 	.corereq_high = false,
 	.sysclkreq_high = true,
-	.suspend_mode = TEGRA_SUSPEND_LP1,
+	.suspend_mode = TEGRA_SUSPEND_LP0,
 };
 
 static void __init __init_debug_uart_D(void)
