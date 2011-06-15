e30091c31cfbf897b2bc2ba3ae0eaa2b3c956c04
Jordan Nien <jnien@nvidia.com>
2011-06-15 14:22:29 +0800

    CHROMIUM:ARM:tegra:kaen: Enable RF for 3G modem on kaen DVT-2
    The changes won't affect kaen DVT-1 because this pin is not connected on kaen DVT-1.
    
    BUG=chrome-os-partner:4506
    TEST=test on Kaen DVT-2 board.
    Using "modem status" to check WWAN status,"Enabled" item should be "true".
    
    Change-Id: I1714c1d0d48161b719316e6c002d1c4d3494f030
    Signed-off-by: Jordan Nien <jnien@nvidia.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/2661
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Vincent Palatin <vpalatin@chromium.org>

--- arch/arm/mach-tegra/board-seaboard.h
+++ arch/arm/mach-tegra/board-seaboard.h
@@ -51,6 +51,7 @@
 #define TEGRA_GPIO_WP_STATUS		TEGRA_GPIO_PH3
 #define TEGRA_GPIO_EN_1V5		SEABOARD_GPIO_TPS6586X(0)
 #define TEGRA_GPIO_RESET		TEGRA_GPIO_PG3
+#define TEGRA_GPIO_W_DISABLE		TEGRA_GPIO_PU4
 
 void seaboard_pinmux_init(void);
 int seaboard_power_init(void);
