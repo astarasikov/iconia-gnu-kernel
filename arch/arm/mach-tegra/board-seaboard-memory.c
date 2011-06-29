d6a7f79fcaec2f273be264ac8d1a27560554cbc6
Dave Parker <dparker@chromium.org>
2011-06-29 11:06:00 -0700

    Revert "CHROMIUM: arm: tegra: To expand and modify DVFS EMC table to support all sku of Aebl-DVT"
    The corresponding BCT file needs additional testing before the EMC table change
    can go in to the kernel.
    
    This reverts commit 8caa41897b4a3db75cc5c476e779a4866c4c791d.
    
    TEST=None
    BUG=None
    TBR=Olof Johansson <olofj@chromium.org>
    
    Change-Id: I702e8c1f67840bac64e048ea77cd29b1d2f86fdb
    Signed-off-by: David Parker <dparker@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/3380
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-tegra/board-seaboard-memory.c
+++ arch/arm/mach-tegra/board-seaboard-memory.c
@@ -268,13 +268,14 @@ void __init seaboard_emc_init(void)
 	}
 }
 
+#ifdef CONFIG_MACH_KAEN
+
 #define STRAP_OPT 0x008
 #define GMI_AD0 (1 << 4)
 #define GMI_AD1 (1 << 5)
 #define RAM_ID_MASK (GMI_AD0 | GMI_AD1)
 #define RAM_CODE_SHIFT 4
 
-#ifdef CONFIG_MACH_KAEN
 static const struct tegra_emc_table kaen_emc_tables_Nanya_333Mhz[] = {
 	{
 		.rate = 166500,   /* SDRAM frequency */
@@ -738,7 +739,7 @@ void __init kaen_emc_init(void)
 #endif
 
 #ifdef CONFIG_MACH_AEBL
-static const struct tegra_emc_table aebl_emc_tables_Micron_380Mhz[] = {
+static const struct tegra_emc_table aebl_emc_tables[] = {
 	{
 		.rate = 190000,   /* SDRAM frequency */
 		.regs = {
@@ -756,7 +757,7 @@ static const struct tegra_emc_table aebl_emc_tables_Micron_380Mhz[] = {
 			0x00000001,   /* REXT */
 			0x00000003,   /* WDV */
 			0x00000004,   /* QUSE */
-			0x00000003,   /* QRST */
+			0x00000005,   /* QRST */
 			0x00000009,   /* QSAFE */
 			0x0000000c,   /* RDV */
 			0x0000059f,   /* REFRESH */
@@ -780,7 +781,7 @@ static const struct tegra_emc_table aebl_emc_tables_Micron_380Mhz[] = {
 			0x00000000,   /* ODT_READ */
 			0x00000083,   /* FBIO_CFG5 */
 			0xa06204ae,   /* CFG_DIG_DLL */
-			0x007e4010,   /* DLL_XFORM_DQS */
+			0x007e8010,   /* DLL_XFORM_DQS */
 			0x00000000,   /* DLL_XFORM_QUSE */
 			0x00000000,   /* ZCAL_REF_CNT */
 			0x00000000,   /* ZCAL_WAIT_CNT */
@@ -842,143 +843,10 @@ static const struct tegra_emc_table aebl_emc_tables_Micron_380Mhz[] = {
 	}
 };
 
-static const struct tegra_emc_table aebl_emc_tables_Hynix_380Mhz[] = {
-	{
-		.rate = 190000,   /* SDRAM frequency */
-		.regs = {
-			0x0000000c,   /* RC */
-			0x00000026,   /* RFC */
-			0x00000009,   /* RAS */
-			0x00000003,   /* RP */
-			0x00000004,   /* R2W */
-			0x00000004,   /* W2R */
-			0x00000002,   /* R2P */
-			0x0000000c,   /* W2P */
-			0x00000003,   /* RD_RCD */
-			0x00000003,   /* WR_RCD */
-			0x00000002,   /* RRD */
-			0x00000001,   /* REXT */
-			0x00000004,   /* WDV */
-			0x00000005,   /* QUSE */
-			0x00000004,   /* QRST */
-			0x00000009,   /* QSAFE */
-			0x0000000d,   /* RDV */
-			0x0000059f,   /* REFRESH */
-			0x00000000,   /* BURST_REFRESH_NUM */
-			0x00000003,   /* PDEX2WR */
-			0x00000003,   /* PDEX2RD */
-			0x00000003,   /* PCHG2PDEN */
-			0x00000003,   /* ACT2PDEN */
-			0x00000001,   /* AR2PDEN */
-			0x0000000b,   /* RW2PDEN */
-			0x000000c8,   /* TXSR */
-			0x00000003,   /* TCKE */
-			0x00000007,   /* TFAW */
-			0x00000004,   /* TRPAB */
-			0x0000000f,   /* TCLKSTABLE */
-			0x00000002,   /* TCLKSTOP */
-			0x00000000,   /* TREFBW */
-			0x00000000,   /* QUSE_EXTRA */
-			0x00000003,   /* FBIO_CFG6 */
-			0x00000000,   /* ODT_WRITE */
-			0x00000000,   /* ODT_READ */
-			0x00000083,   /* FBIO_CFG5 */
-			0xa06204ae,   /* CFG_DIG_DLL */
-			0x007e0010,   /* DLL_XFORM_DQS */
-			0x00000000,   /* DLL_XFORM_QUSE */
-			0x00000000,   /* ZCAL_REF_CNT */
-			0x00000000,   /* ZCAL_WAIT_CNT */
-			0x00000000,   /* AUTO_CAL_INTERVAL */
-			0x00000000,   /* CFG_CLKTRIM_0 */
-			0x00000000,   /* CFG_CLKTRIM_1 */
-			0x00000000,   /* CFG_CLKTRIM_2 */
-		}
-	}, {
-		.rate = 380000,   /* SDRAM frequency */
-		.regs = {
-			0x00000017,   /* RC */
-			0x0000004b,   /* RFC */
-			0x00000012,   /* RAS */
-			0x00000006,   /* RP */
-			0x00000004,   /* R2W */
-			0x00000005,   /* W2R */
-			0x00000003,   /* R2P */
-			0x0000000c,   /* W2P */
-			0x00000006,   /* RD_RCD */
-			0x00000006,   /* WR_RCD */
-			0x00000003,   /* RRD */
-			0x00000001,   /* REXT */
-			0x00000004,   /* WDV */
-			0x00000005,   /* QUSE */
-			0x00000004,   /* QRST */
-			0x00000009,   /* QSAFE */
-			0x0000000d,   /* RDV */
-			0x00000b5f,   /* REFRESH */
-			0x00000000,   /* BURST_REFRESH_NUM */
-			0x00000003,   /* PDEX2WR */
-			0x00000003,   /* PDEX2RD */
-			0x00000006,   /* PCHG2PDEN */
-			0x00000006,   /* ACT2PDEN */
-			0x00000001,   /* AR2PDEN */
-			0x00000011,   /* RW2PDEN */
-			0x000000c8,   /* TXSR */
-			0x00000003,   /* TCKE */
-			0x0000000e,   /* TFAW */
-			0x00000007,   /* TRPAB */
-			0x0000000f,   /* TCLKSTABLE */
-			0x00000002,   /* TCLKSTOP */
-			0x00000000,   /* TREFBW */
-			0x00000000,   /* QUSE_EXTRA */
-			0x00000003,   /* FBIO_CFG6 */
-			0x00000000,   /* ODT_WRITE */
-			0x00000000,   /* ODT_READ */
-			0x00000083,   /* FBIO_CFG5 */
-			0xe044048b,   /* CFG_DIG_DLL */
-			0x007da010,   /* DLL_XFORM_DQS */
-			0x00000000,   /* DLL_XFORM_QUSE */
-			0x00000000,   /* ZCAL_REF_CNT */
-			0x00000000,   /* ZCAL_WAIT_CNT */
-			0x00000000,   /* AUTO_CAL_INTERVAL */
-			0x00000000,   /* CFG_CLKTRIM_0 */
-			0x00000000,   /* CFG_CLKTRIM_1 */
-			0x00000000,   /* CFG_CLKTRIM_2 */
-		}
-	}
-};
-
-struct tegra_board_emc_table aebl_emc[] = {
-	{
-		.table		= aebl_emc_tables_Micron_380Mhz,
-		.table_size	= ARRAY_SIZE(aebl_emc_tables_Micron_380Mhz),
-		.name		= "Mircron 380MHz",
-	},
-	{
-		.table		= aebl_emc_tables_Hynix_380Mhz,
-		.table_size	= ARRAY_SIZE(aebl_emc_tables_Hynix_380Mhz),
-		.name		= "Hynix 380MHz",
-	},
-};
-
 void __init aebl_emc_init(void)
 {
-	u32 reg;
-	int ram_id;
-	void __iomem *apb_misc = IO_ADDRESS(TEGRA_APB_MISC_BASE);
-
 	BUG_ON(!machine_is_aebl());
-
-	/* TODO: Move this to use the apbio library in 2.6.38 */
-	reg = readl(apb_misc + STRAP_OPT);
-	ram_id = (reg & RAM_ID_MASK) >> RAM_CODE_SHIFT;
-
-	if (ram_id >= ARRAY_SIZE(aebl_emc) || !aebl_emc[ram_id].table) {
-		pr_err("EMC table for ram id %d not found. " \
-		       "System stability might be compromised\n", ram_id);
-	} else {
-		pr_info("Tegra EMC table in use: %s\n", aebl_emc[ram_id].name);
-		tegra_init_emc(aebl_emc[ram_id].table,
-			       aebl_emc[ram_id].table_size);
-	}
+	tegra_init_emc(aebl_emc_tables, ARRAY_SIZE(aebl_emc_tables));
 }
 #endif
 
