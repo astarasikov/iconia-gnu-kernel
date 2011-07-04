591a54934d543faec45459994a40c5ce0482c75a
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-07-05 01:35:45 +0400

    Add power off routine for Iconia A500

--- arch/arm/mach-tegra/board-picasso.c
+++ arch/arm/mach-tegra/board-picasso.c
@@ -35,6 +35,7 @@
 #include <mach/usb_phy.h>
 #include <linux/platform_data/tegra_usb.h>
 #include <linux/input.h>
+#include <linux/regulator/consumer.h>
 
 #include <mach/iomap.h>
 #include <mach/irqs.h>
@@ -234,7 +235,44 @@ static void __init picasso_touch_init(void) {
 
 	i2c_register_board_info(0, &mxt_device, 1);
 }
+/******************************************************************************
+ * Power Supply
+ *****************************************************************************/
+static void reg_off(const char *reg)
+{
+	int rc;
+	struct regulator *regulator;
 
+	regulator = regulator_get(NULL, reg);
+
+	if (IS_ERR(regulator)) {
+		pr_err("%s: regulator_get returned %ld\n", __func__,
+		       PTR_ERR(regulator));
+		return;
+	}
+
+	rc = regulator_force_disable(regulator);
+	if (rc)
+		pr_err("%s: regulator_force_disable returned %d\n", __func__,
+			rc);
+	regulator_put(regulator);
+}
+
+static void picasso_power_off(void)
+{
+	reg_off("vdd_sm2");
+	reg_off("vdd_core");
+	reg_off("vdd_cpu");
+	local_irq_disable();
+	while (1) {
+		dsb();
+		__asm__ ("wfi");
+	}
+}
+
+static void __init picasso_power_init(void) {
+	pm_power_off = picasso_power_off;
+}
 /******************************************************************************
  * I2C
  *****************************************************************************/
@@ -411,6 +449,7 @@ static void __init tegra_picasso_init(void)
 	picasso_emc_init();
 	picasso_i2c_init();
 	picasso_regulator_init();
+	picasso_power_init();
 	picasso_usb_init();
 	picasso_keys_init();
 	picasso_panel_init();
