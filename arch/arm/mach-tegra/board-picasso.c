ca76effdd496c3ce4bef8dbcc7d0776c6742aaed
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-07-05 01:52:26 +0400

    Change Iconia rfkill key to switch

--- arch/arm/mach-tegra/board-picasso.c
+++ arch/arm/mach-tegra/board-picasso.c
@@ -364,7 +364,7 @@ static struct gpio_keys_button picasso_keys[] = {
 		.debounce_interval = 10,
 	},
 	{
-		.code = KEY_RFKILL,
+		.code = SW_RFKILL_ALL,
 		.gpio = PICASSO_GPIO_SWITCH_LOCK,
 		.desc = "Lock Switch",
 		.type = EV_SW,
