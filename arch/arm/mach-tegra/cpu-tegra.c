1cfa2beae026a0b5ff3130b9590c6f6850d11153
Marc Dietrich <marvin24@gmx.de>
2011-08-21 20:54:39 +0200

    paz00: fix possible typo in cpu-tegra.c
    Latency is measured in nano-secs, not mu-secs. Found by juliank
    on IRC.

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -389,7 +389,7 @@ static int tegra_cpu_init(struct cpufreq_policy *policy)
 	target_cpu_speed[policy->cpu] = policy->cur;
 
 	/* cpu clock change latency: ~400us */
-	policy->cpuinfo.transition_latency = 400;
+	policy->cpuinfo.transition_latency = 400 * 1000;
 
 	policy->shared_type = CPUFREQ_SHARED_TYPE_ALL;
 	cpumask_copy(policy->related_cpus, cpu_possible_mask);
