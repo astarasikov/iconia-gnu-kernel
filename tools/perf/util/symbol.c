176bbcdaa37f6f521a46ef09cb147f0dbf89f850
Anton Blanchard <anton@samba.org>
2011-08-24 16:40:15 +1000

    UPSTREAM: perf symbols: /proc/kallsyms does not sort module symbols
    kallsyms__parse assumes that /proc/kallsyms is sorted and sets the end
    of the previous symbol to the start of the current one.
    
    Unfortunately module symbols are not sorted, eg:
    
    ffffffffa0081f30 t e1000_clean_rx_irq   [e1000e]
    ffffffffa00817a0 t e1000_alloc_rx_buffers       [e1000e]
    
    Some symbols end up with a negative length and others have a length
    larger than they should. This results in confusing perf output.
    
    We already have a function to fixup the end of zero length symbols so
    use that instead.
    
    Cc: Eric B Munson <emunson@mgebm.net>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/20110824065242.969681349@samba.org
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    BUG=none
    TEST=none
    
    upstream SHA1 3f5a42722b9e78a434d5a4ee5e607dc33c69ac80
    
    Change-Id: I2bcbc3343babe8fced33024af73f5c28fcc670ff
    Reviewed-on: http://gerrit.chromium.org/gerrit/8592
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Ready: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -436,17 +436,10 @@ int kallsyms__parse(const char *filename, void *arg,
 	char *line = NULL;
 	size_t n;
 	int err = -1;
-	u64 prev_start = 0;
-	char prev_symbol_type = 0;
-	char *prev_symbol_name;
 	FILE *file = fopen(filename, "r");
 
 	if (file == NULL)
 		goto out_failure;
-
-	prev_symbol_name = malloc(KSYM_NAME_LEN);
-	if (prev_symbol_name == NULL)
-		goto out_close;
 
 	err = 0;
 
@@ -478,24 +471,18 @@ int kallsyms__parse(const char *filename, void *arg,
 			break;
 		}
 
-		if (prev_symbol_type) {
-			u64 end = start;
-			if (end != prev_start)
-				--end;
-			err = process_symbol(arg, prev_symbol_name,
-					     prev_symbol_type, prev_start, end);
-			if (err)
-				break;
-		}
-
-		memcpy(prev_symbol_name, symbol_name, len + 1);
-		prev_symbol_type = symbol_type;
-		prev_start = start;
+		/*
+		 * module symbols are not sorted so we add all
+		 * symbols with zero length and rely on
+		 * symbols__fixup_end() to fix it up.
+		 */
+		err = process_symbol(arg, symbol_name,
+				     symbol_type, start, start);
+		if (err)
+			break;
 	}
 
-	free(prev_symbol_name);
 	free(line);
-out_close:
 	fclose(file);
 	return err;
 
@@ -674,18 +661,20 @@ discard_symbol:		rb_erase(&pos->rb_node, root);
 	return count + moved;
 }
 
-int dso__load_kallsyms(struct dso *self, const char *filename,
+int dso__load_kallsyms(struct dso *dso, const char *filename,
 		       struct map *map, symbol_filter_t filter)
 {
-	if (dso__load_all_kallsyms(self, filename, map) < 0)
+	if (dso__load_all_kallsyms(dso, filename, map) < 0)
 		return -1;
 
-	if (self->kernel == DSO_TYPE_GUEST_KERNEL)
-		self->origin = DSO__ORIG_GUEST_KERNEL;
-	else
-		self->origin = DSO__ORIG_KERNEL;
+	symbols__fixup_end(&dso->symbols[map->type]);
 
-	return dso__split_kallsyms(self, map, filter);
+	if (dso->kernel == DSO_TYPE_GUEST_KERNEL)
+		dso->origin = DSO__ORIG_GUEST_KERNEL;
+	else
+		dso->origin = DSO__ORIG_KERNEL;
+
+	return dso__split_kallsyms(dso, map, filter);
 }
 
 static int dso__load_perf_map(struct dso *self, struct map *map,
