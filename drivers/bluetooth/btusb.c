2e313a96cf39f3c9155a1e424d6d0f6e7f37ad65
Jay Kim <yongjaek@google.com>
2011-08-25 11:10:48 -0700

    BACKPORT: Bluetooth: Add firmware support for Atheros 3012
    Blacklisted AR3012 PID in btusb and added the same
    in ath3k to load patch and sysconfig files.
    
    BUG=chrome-os-partner:5499
    TEST=hcitool scan
    
    Signed-off-by: Bala Shanmugam <sbalashanmugam@atheros.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    (cherry picked from commit d9f51b51db2064c9049bf7924318fd8c6ed852cb)
    
    Conflicts:
    
    	drivers/bluetooth/ath3k.c
    	drivers/bluetooth/btusb.c
    
    Change-Id: I427c5564b27a99b543be06b45bfc0b641e0d2bef
    Reviewed-on: http://gerrit.chromium.org/gerrit/6659
    Tested-by: Jay Kim <yongjaek@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -111,6 +111,9 @@ static struct usb_device_id blacklist_table[] = {
 	/* Atheros AR5BBU12 with sflash firmware */
 	{ USB_DEVICE(0x0489, 0xe02c), .driver_info = BTUSB_IGNORE },
 
+	/* Atheros 3012 with sflash firmware */
+	{ USB_DEVICE(0x0cf3, 0x3004), .driver_info = BTUSB_IGNORE },
+
 	/* Broadcom BCM2035 */
 	{ USB_DEVICE(0x0a5c, 0x2035), .driver_info = BTUSB_WRONG_SCO_MTU },
 	{ USB_DEVICE(0x0a5c, 0x200a), .driver_info = BTUSB_WRONG_SCO_MTU },
