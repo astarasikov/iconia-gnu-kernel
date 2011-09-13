RonGokhale/codeaurora_kernel

CHROMIUM: arm: tegra: Add support for HDMI on Seaboard.
This adds an HDMI platform device as well as hooks to enable and disable
power to the HDMI block.

In addition, it reconfigures some pinmux settings to allow HDMI to work
properly:
- Initialize DDC and PTA groups to their "safe" settings (since I2C2 is
  multiplexed between them, the tegra-i2c driver will take care of
  toggling the muxes and reset to "safe" when not in use).
- Enable normal tristate mode on DDC group.
- Enable normal tristate modet on HDINT pingroup (used for hotplug
  detection).

BUG=chrome-os-partner:1812
TEST=HDMI on Seaboard, including hotplug and dynamic transitions on and off
Signed-off-by: Robert Morell <rmorell@nvidia.com>
Signed-off-by: Olof Johansson <olofj@chromium.org>

Review URL: http://codereview.chromium.org/6628030

Change-Id: I0e40489dfac8c5968fac6d793568844d27782c02

arch/arm/mach-tegra/board-seaboard-panel.c
@@ -116,6 +116,31 @@ static int seaboard_panel_disable(void)
 	return 0;
 }
 
+static struct regulator *seaboard_hdmi_reg;
+static struct regulator *seaboard_hdmi_pll;
+
+static int seaboard_hdmi_enable(void)
+{
+	if (WARN_ON(!seaboard_hdmi_reg || !seaboard_hdmi_pll))
+		return -ENODEV;
+
+	gpio_set_value(seaboard_hdmi_enb, 1);
+	regulator_enable(seaboard_hdmi_reg);
+	regulator_enable(seaboard_hdmi_pll);
+	return 0;
+}
+
+static int seaboard_hdmi_disable(void)
+{
+	if (WARN_ON(!seaboard_hdmi_reg || !seaboard_hdmi_pll))
+		return -ENODEV;
+
+	gpio_set_value(seaboard_hdmi_enb, 0);
+	regulator_disable(seaboard_hdmi_reg);
+	regulator_disable(seaboard_hdmi_pll);
+	return 0;
+}
+
 static struct resource seaboard_disp1_resources[] = {
 	{
 		.name	= "irq",
@@ -133,6 +158,27 @@ static struct resource seaboard_disp1_resources[] = {
 		.name	= "fbmem",
 		.start	= 0x18012000,
 		.end	= 0x18012000 + 0x402000 - 1, /* enough for 1368*768 16bpp */
+		.flags	= IORESOURCE_MEM,
+	},
+};
+
+static struct resource seaboard_disp2_resources[] = {
+	{
+		.name	= "irq",
+		.start	= INT_DISPLAY_B_GENERAL,
+		.end	= INT_DISPLAY_B_GENERAL,
+		.flags	= IORESOURCE_IRQ,
+	},
+	{
+		.name	= "regs",
+		.start	= TEGRA_DISPLAY2_BASE,
+		.end	= TEGRA_DISPLAY2_BASE + TEGRA_DISPLAY2_SIZE - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.name	= "hdmi_regs",
+		.start	= TEGRA_HDMI_BASE,
+		.end	= TEGRA_HDMI_BASE + TEGRA_HDMI_SIZE - 1,
 		.flags	= IORESOURCE_MEM,
 	},
 };
@@ -183,6 +229,13 @@ static struct tegra_fb_data wario_fb_data = {
 	.bits_per_pixel	= 16,
 };
 
+static struct tegra_fb_data seaboard_hdmi_fb_data = {
+	.win		= 0,
+	.xres		= 1280,
+	.yres		= 720,
+	.bits_per_pixel	= 16,
+};
+
 static struct tegra_dc_out seaboard_disp1_out = {
 	.type		= TEGRA_DC_OUT_RGB,
 
@@ -196,11 +249,31 @@ static struct tegra_dc_out seaboard_disp1_out = {
 	.disable	= seaboard_panel_disable,
 };
 
+static struct tegra_dc_out seaboard_disp2_out = {
+	.type		= TEGRA_DC_OUT_HDMI,
+	.flags		= TEGRA_DC_OUT_HOTPLUG_HIGH,
+
+	.dcc_bus	= 1,
+	.hotplug_gpio	= seaboard_hdmi_hpd,
+
+	.align		= TEGRA_DC_ALIGN_MSB,
+	.order		= TEGRA_DC_ORDER_RED_BLUE,
+
+	.enable		= seaboard_hdmi_enable,
+	.disable	= seaboard_hdmi_disable,
+};
+
 static struct tegra_dc_platform_data seaboard_disp1_pdata = {
 	.flags		= TEGRA_DC_FLAG_ENABLED,
 	.default_out	= &seaboard_disp1_out,
 	.fb		= &seaboard_fb_data,
 	.emc_clk_rate	= 300000000,
+};
+
+static struct tegra_dc_platform_data seaboard_disp2_pdata = {
+	.flags		= 0,
+	.default_out	= &seaboard_disp2_out,
+	.fb		= &seaboard_hdmi_fb_data,
 };
 
 static struct nvhost_device seaboard_disp1_device = {
@@ -210,6 +283,16 @@ static struct nvhost_device seaboard_disp1_device = {
 	.num_resources	= ARRAY_SIZE(seaboard_disp1_resources),
 	.dev = {
 		.platform_data = &seaboard_disp1_pdata,
+	},
+};
+
+static struct nvhost_device seaboard_disp2_device = {
+	.name		= "tegradc",
+	.id		= 1,
+	.resource	= seaboard_disp2_resources,
+	.num_resources	= ARRAY_SIZE(seaboard_disp2_resources),
+	.dev = {
+		.platform_data = &seaboard_disp2_pdata,
 	},
 };
 
@@ -267,7 +350,7 @@ int __init seaboard_panel_init(void)
 	tegra_gpio_enable(seaboard_lvds_shutdown);
 
 	gpio_request(seaboard_hdmi_enb, "hdmi_5v_en");
-	gpio_direction_output(seaboard_hdmi_enb, 0);
+	gpio_direction_output(seaboard_hdmi_enb, 1);
 	tegra_gpio_enable(seaboard_hdmi_enb);
 
 	gpio_request(seaboard_hdmi_hpd, "hdmi_hpd");
@@ -285,9 +368,42 @@ int __init seaboard_panel_init(void)
 	if (!err)
 		err = nvhost_device_register(&seaboard_disp1_device);
 
-/*	if (!err)
+	if (!err)
 		err = nvhost_device_register(&seaboard_disp2_device);
-*/
+
 	return err;
 }
 
+static int __init seaboard_hdmi_late_init(void)
+{
+	int ret;
+
+	seaboard_hdmi_reg = regulator_get(NULL, "avdd_hdmi");
+	if (IS_ERR_OR_NULL(seaboard_hdmi_reg)) {
+		ret = PTR_ERR(seaboard_hdmi_reg);
+		goto fail;
+	}
+
+	seaboard_hdmi_pll = regulator_get(NULL, "avdd_hdmi_pll");
+	if (IS_ERR_OR_NULL(seaboard_hdmi_pll)) {
+		ret = PTR_ERR(seaboard_hdmi_pll);
+		goto fail;
+	}
+
+	return 0;
+
+fail:
+	if (seaboard_hdmi_pll) {
+		regulator_disable(seaboard_hdmi_pll);
+		seaboard_hdmi_pll = NULL;
+	}
+
+	if (seaboard_hdmi_reg) {
+		regulator_disable(seaboard_hdmi_reg);
+		seaboard_hdmi_reg = NULL;
+	}
+
+	return ret;
+}
+
+late_initcall(seaboard_hdmi_late_init);

astarasikov/iconia-gnu-kernel

arch/arm/mach-tegra/board-seaboard-panel.c
@@ -97,6 +97,31 @@ static int seaboard_panel_disable(void)
 	return 0;
 }
 
+static struct regulator *seaboard_hdmi_reg;
+static struct regulator *seaboard_hdmi_pll;
+
+static int seaboard_hdmi_enable(void)
+{
+	if (WARN_ON(!seaboard_hdmi_reg || !seaboard_hdmi_pll))
+		return -ENODEV;
+
+	gpio_set_value(TEGRA_GPIO_HDMI_ENB, 1);
+	regulator_enable(seaboard_hdmi_reg);
+	regulator_enable(seaboard_hdmi_pll);
+	return 0;
+}
+
+static int seaboard_hdmi_disable(void)
+{
+	if (WARN_ON(!seaboard_hdmi_reg || !seaboard_hdmi_pll))
+		return -ENODEV;
+
+	gpio_set_value(TEGRA_GPIO_HDMI_ENB, 0);
+	regulator_disable(seaboard_hdmi_reg);
+	regulator_disable(seaboard_hdmi_pll);
+	return 0;
+}
+
 static struct resource seaboard_disp1_resources[] = {
 	{
 		.name	= "irq",
@@ -114,6 +139,27 @@ static struct resource seaboard_disp1_resources[] = {
 		.name	= "fbmem",
 		.start	= 0x18012000,
 		.end	= 0x18012000 + 0x402000 - 1, /* enough for 1368*768 16bpp */
+		.flags	= IORESOURCE_MEM,
+	},
+};
+
+static struct resource seaboard_disp2_resources[] = {
+	{
+		.name	= "irq",
+		.start	= INT_DISPLAY_B_GENERAL,
+		.end	= INT_DISPLAY_B_GENERAL,
+		.flags	= IORESOURCE_IRQ,
+	},
+	{
+		.name	= "regs",
+		.start	= TEGRA_DISPLAY2_BASE,
+		.end	= TEGRA_DISPLAY2_BASE + TEGRA_DISPLAY2_SIZE - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.name	= "hdmi_regs",
+		.start	= TEGRA_HDMI_BASE,
+		.end	= TEGRA_HDMI_BASE + TEGRA_HDMI_SIZE - 1,
 		.flags	= IORESOURCE_MEM,
 	},
 };
@@ -164,6 +210,13 @@ static struct tegra_fb_data wario_fb_data = {
 	.bits_per_pixel	= 16,
 };
 
+static struct tegra_fb_data seaboard_hdmi_fb_data = {
+	.win		= 0,
+	.xres		= 1280,
+	.yres		= 720,
+	.bits_per_pixel	= 16,
+};
+
 static struct tegra_dc_out seaboard_disp1_out = {
 	.type		= TEGRA_DC_OUT_RGB,
 
@@ -177,11 +230,31 @@ static struct tegra_dc_out seaboard_disp1_out = {
 	.disable	= seaboard_panel_disable,
 };
 
+static struct tegra_dc_out seaboard_disp2_out = {
+	.type		= TEGRA_DC_OUT_HDMI,
+	.flags		= TEGRA_DC_OUT_HOTPLUG_HIGH,
+
+	.dcc_bus	= 1,
+	.hotplug_gpio	= TEGAR_GPIO_HDMI_HPD,
+
+	.align		= TEGRA_DC_ALIGN_MSB,
+	.order		= TEGRA_DC_ORDER_RED_BLUE,
+
+	.enable		= seaboard_hdmi_enable,
+	.disable	= seaboard_hdmi_disable,
+};
+
 static struct tegra_dc_platform_data seaboard_disp1_pdata = {
 	.flags		= TEGRA_DC_FLAG_ENABLED,
 	.default_out	= &seaboard_disp1_out,
 	.fb		= &seaboard_fb_data,
 	.emc_clk_rate	= 300000000,
+};
+
+static struct tegra_dc_platform_data seaboard_disp2_pdata = {
+	.flags		= 0,
+	.default_out	= &seaboard_disp2_out,
+	.fb		= &seaboard_hdmi_fb_data,
 };
 
 static struct nvhost_device seaboard_disp1_device = {
@@ -198,6 +271,16 @@ static int seaboard_disp1_check_fb(struct device *dev, struct fb_info *info)
 {
 	return info->device == &seaboard_disp1_device.dev;
 }
+
+static struct nvhost_device seaboard_disp2_device = {
+	.name		= "tegradc",
+	.id		= 1,
+	.resource	= seaboard_disp2_resources,
+	.num_resources	= ARRAY_SIZE(seaboard_disp2_resources),
+	.dev = {
+		.platform_data = &seaboard_disp2_pdata,
+	},
+};
 
 static struct nvmap_platform_carveout seaboard_carveouts[] = {
 	[0] = {
@@ -246,6 +329,9 @@ int __init seaboard_panel_init(void)
 	gpio_request(TEGRA_GPIO_BACKLIGHT_VDD, "bl_vdd");
 	gpio_direction_output(TEGRA_GPIO_BACKLIGHT_VDD, 1);
 
+	gpio_request(TEGRA_GPIO_HDMI_ENB, "hdmi_5v_en");
+	gpio_direction_output(TEGRA_GPIO_HDMI_ENB, 1);
+
 	gpio_request(TEGRA_GPIO_LVDS_SHUTDOWN, "lvds_shdn");
 	gpio_direction_output(TEGRA_GPIO_LVDS_SHUTDOWN, 1);
 
@@ -260,6 +346,42 @@ int __init seaboard_panel_init(void)
 	if (!err)
 		err = nvhost_device_register(&seaboard_disp1_device);
 
+	if (!err)
+		err = nvhost_device_register(&seaboard_disp2_device);
+
 	return err;
 }
 
+static int __init seaboard_hdmi_late_init(void)
+{
+	int ret;
+
+	seaboard_hdmi_reg = regulator_get(NULL, "avdd_hdmi");
+	if (IS_ERR_OR_NULL(seaboard_hdmi_reg)) {
+		ret = PTR_ERR(seaboard_hdmi_reg);
+		goto fail;
+	}
+
+	seaboard_hdmi_pll = regulator_get(NULL, "avdd_hdmi_pll");
+	if (IS_ERR_OR_NULL(seaboard_hdmi_pll)) {
+		ret = PTR_ERR(seaboard_hdmi_pll);
+		goto fail;
+	}
+
+	return 0;
+
+fail:
+	if (seaboard_hdmi_pll) {
+		regulator_disable(seaboard_hdmi_pll);
+		seaboard_hdmi_pll = NULL;
+	}
+
+	if (seaboard_hdmi_reg) {
+		regulator_disable(seaboard_hdmi_reg);
+		seaboard_hdmi_reg = NULL;
+	}
+
+	return ret;
+}
+
+late_initcall(seaboard_hdmi_late_init);