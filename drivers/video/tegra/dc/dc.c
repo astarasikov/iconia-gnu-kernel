ca0fd5a3125aa362f9f8d04e98e3398cf47ed0e0
Dilan Lee <dilee@nvidia.com>
2011-09-21 14:37:47 +0800

    CHROMIUM: arm: tegra: invoke dc->out->disable before dc->out_ops->disable
    It would fix display noise when disabling DC output.
    In original code, it will disable dc and then disable
    display interface(LVDS or HDMI).
    After dc disabled, maybe there are unknow signals on hardware bus,
    it leads display noise until dispay interface disabled.
    
    BUG=chrome-os-partner:5853
    BUG=chrome-os-partner:5622
    TEST=passed more than 100 times of display resolution changed
         without any display noise.
    
    Change-Id: If0182a64f2515b372394c917c07a8fbd1c5a4a84
    Signed-off-by: Dilan Lee <dilee@nvidia.com>
    Reviewed-on: http://gerrit.chromium.org/gerrit/8040
    Reviewed-by: Robert Morell <rmorell@nvidia.com>
    Commit-Ready: Olof Johansson <olofj@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1403,9 +1403,6 @@ static bool _tegra_dc_enable(struct tegra_dc *dc)
 
 	tegra_dc_io_start(dc);
 
-	if (dc->out && dc->out->enable)
-		dc->out->enable();
-
 	tegra_dc_setup_clk(dc, dc->clk);
 
 	clk_enable(dc->clk);
@@ -1419,6 +1416,9 @@ static bool _tegra_dc_enable(struct tegra_dc *dc)
 
 	if (dc->out_ops && dc->out_ops->enable)
 		dc->out_ops->enable(dc);
+
+	if (dc->out && dc->out->enable)
+		dc->out->enable();
 
 	/* force a full blending update */
 	dc->blend.z[0] = -1;
@@ -1449,15 +1449,15 @@ static void _tegra_dc_disable(struct tegra_dc *dc)
 
 	disable_irq(dc->irq);
 
+	if (dc->out && dc->out->disable)
+		dc->out->disable();
+
 	if (dc->out_ops && dc->out_ops->disable)
 		dc->out_ops->disable(dc);
 
 	clk_disable(dc->emc_clk);
 	clk_disable(dc->clk);
 	tegra_dvfs_set_rate(dc->clk, 0);
-
-	if (dc->out && dc->out->disable)
-		dc->out->disable();
 
 	/* flush any pending syncpt waits */
 	for (i = 0; i < dc->n_windows; i++) {
