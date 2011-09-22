4c5a21df31782214ec5e8db4b981d5606c519968
Doug Anderson <dianders@chromium.org>
2011-09-22 12:01:24 -0700

    CHROMIUM: tegra: Make earlyprintk choose a UART at runtime.
    With this change we automatically detect which UART to use for
    earlyprintk (and for printing during decompression).  The
    detection involves coordination with the bootloader: it's expected
    that the bootloader will leave a 'D' in the UART scratchpad
    register for whatever UART we should use for debugging.
    
    If we don't find any UARTs that were specified, we'll fall back to
    the UART that was specified during config time:
    CONFIG_TEGRA_DEBUG_UART_XXX.
    
    As a side effect of this change, earlyprintk will no longer fail
    if you've specified CONFIG_TEGRA_DEBUG_UART_NONE.
    
    BUG=chromium-os:19656
    TEST=Manual
    1. Applied future change to set CONFIG_TEGRA_DEBUG_UART_NONE.
    2. Applied future change to u-boot set a 'D' in the scratchpad of
       the right UART.
    3. Installed legacy u-boot and made sure I had earlyprintk by doing
       setenv dev_extras earlyprintk
       saveenv
    4. Booted Searboard.  Saw the "Uncompressing Linux..." message as
       well as early printouts.
    5. Rebooted, holding space on serial console while booting to drop
       into u-boot.
    6. Manually unset the scratchpad register on UART D:
       md.l 0x7000631c 1
       mw.l 0x7000631c ' '
       md.l 0x7000631c 1
    7. Booted:
       boot
    8. Saw no early printouts.
    
    I also tried changing the default kernel config to UARTD.  Then and
    repeated steps #1-#7.  In this case, in step #8 I still saw all
    of the messages on the console.
    
    Change-Id: Icc3204022dc440f78ff796c0a59f48884ab1cca4
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/8148
    Reviewed-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/include/mach/uncompress.h
+++ arch/arm/mach-tegra/include/mach/uncompress.h
@@ -1,11 +1,12 @@
 /*
  * arch/arm/mach-tegra/include/mach/uncompress.h
  *
- * Copyright (C) 2010 Google, Inc.
+ * Copyright (C) 2011 Google, Inc.
  *
  * Author:
  *	Colin Cross <ccross@google.com>
  *	Erik Gilling <konkers@google.com>
+ *	Doug Anderson <dianders@chromium.org>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -21,30 +22,63 @@
 #ifndef __MACH_TEGRA_UNCOMPRESS_H
 #define __MACH_TEGRA_UNCOMPRESS_H
 
+#include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/serial_reg.h>
 
 #include <mach/iomap.h>
 
+u32 uart_base;
+
+#define DEBUG_UART_SHIFT	2
+
 static void putc(int c)
 {
-	volatile u8 *uart = (volatile u8 *)TEGRA_DEBUG_UART_BASE;
-	int shift = 2;
+	volatile u8 *uart = (volatile u8 *)uart_base;
 
 	if (uart == NULL)
 		return;
 
-	while (!(uart[UART_LSR << shift] & UART_LSR_THRE))
+	while (!(uart[UART_LSR << DEBUG_UART_SHIFT] & UART_LSR_THRE))
 		barrier();
-	uart[UART_TX << shift] = c;
+	uart[UART_TX << DEBUG_UART_SHIFT] = c;
 }
 
 static inline void flush(void)
 {
 }
 
+/*
+ * Setup before decompression.  This is where we do UART selection for
+ * earlyprintk and init the uart_base register.
+ */
 static inline void arch_decomp_setup(void)
 {
+	static const u32 uarts[] = {
+		TEGRA_UARTA_BASE,
+		TEGRA_UARTB_BASE,
+		TEGRA_UARTC_BASE,
+		TEGRA_UARTD_BASE,
+		TEGRA_UARTE_BASE,
+	};
+	const u32 num_uarts = ARRAY_SIZE(uarts);
+	u32 i;
+
+	/*
+	 * Look for the first UART that has a 'D' in the scratchpad register,
+	 * which should be set by the bootloader to tell us which UART to use
+	 * for debugging.  If nothing found, we'll fall back to what's
+	 * specified in TEGRA_DEBUG_UART_BASE.
+	 */
+	uart_base = TEGRA_DEBUG_UART_BASE;
+	for (i = 0; i < num_uarts; i++) {
+		volatile u8 *uart = (volatile u8 *)uarts[i];
+
+		if (uart[UART_SCR << DEBUG_UART_SHIFT] == 'D') {
+			uart_base = uarts[i];
+			break;
+		}
+	}
 }
 
 static inline void arch_decomp_wdog(void)
