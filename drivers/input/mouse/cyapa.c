9b7e0df2e336a3d4704fb75240baf863e334f25a
Benson Leung <bleung@chromium.org>
2011-10-22 22:21:20 -0700

    Input: cyapa - SMBus support
    Support for Cypress APA trackpads using
    the SMBus protocol.
    
    The i2c/smbus adapter can be queried for functionality,
    and we can select the appropriate type.
    
    In the case where both are supported by the interface, the
    driver will prefer i2c. A future change may detect failures in i2c
    mode and switch to smbus if both are supported by the adapter in
    cyapa_get_trackpad_run_mode.
    
    BUG=chrome-os-partner:5698
    TEST=Test on i2c cyapa pad. Make sure no functional
    difference after this change.
    Test on Smbus cyapa pad on a Smbus system. Make sure
    that the pad is functional in both cold boot and
    warm boot scenarios.
    
    Change-Id: I62d133ab82cb0db037004f62cd8c787a8e2f6006
    Signed-off-by: Benson Leung <bleung@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/10560
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/input/mouse/cyapa.c
+++ drivers/input/mouse/cyapa.c
@@ -45,7 +45,7 @@
 
 /* commands for read/write registers of Cypress trackpad */
 #define CYAPA_CMD_SOFT_RESET       0x00
-#define CYAPA_CMD_IO_SYNC          0x01
+#define CYAPA_CMD_POWER_MODE       0x01
 #define CYAPA_CMD_DEV_STATUS       0x02
 #define CYAPA_CMD_GROUP_DATA       0x03
 #define CYAPA_CMD_GROUP_CTRL       0x04
@@ -249,6 +249,9 @@ struct cyapa {
 	/* synchronize access to dwork. */
 	spinlock_t lock;
 	int irq;
+	u8 adapter_func;
+	bool smbus;
+	bool touchpad_protocol;
 
 	struct cyapa_mt_slot mt_slots[CYAPA_MAX_MT_SLOTS];
 
@@ -287,6 +290,63 @@ struct cyapa_cmd_len {
 	unsigned char len;
 };
 
+#define CYAPA_ADAPTER_FUNC_NONE   0
+#define CYAPA_ADAPTER_FUNC_I2C    1
+#define CYAPA_ADAPTER_FUNC_SMBUS  2
+#define CYAPA_ADAPTER_FUNC_BOTH   3
+
+#define CYTP_I2C 0
+#define CYTP_SMBUS 1
+
+/*
+ * macros for SMBus communication
+ */
+#define SMBUS_READ   0x01
+#define SMBUS_WRITE 0x00
+#define SMBUS_ENCODE_IDX(cmd, idx) ((cmd) | (((idx) & 0x03) << 1))
+#define SMBUS_ENCODE_RW(cmd, rw) ((cmd) | ((rw) & 0x01))
+#define SMBUS_BYTE_BLOCK_CMD_MASK 0x80
+#define SMBUS_GROUP_BLOCK_CMD_MASK 0x40
+
+ /* for byte read/write command */
+#define CMD_RESET 0
+#define CMD_POWER_MODE 1
+#define CMD_DEV_STATUS 2
+#define SMBUS_BYTE_CMD(cmd) (((cmd) & 0x3F) << 1)
+#define CYAPA_SMBUS_RESET SMBUS_BYTE_CMD(CMD_RESET)
+#define CYAPA_SMBUS_POWER_MODE SMBUS_BYTE_CMD(CMD_POWER_MODE)
+#define CYAPA_SMBUS_DEV_STATUS SMBUS_BYTE_CMD(CMD_DEV_STATUS)
+
+ /* for group registers read/write command */
+#define REG_GROUP_DATA 0
+#define REG_GROUP_CTRL 1
+#define REG_GROUP_CMD 2
+#define REG_GROUP_QUERY 3
+#define SMBUS_GROUP_CMD(grp) (0x80 | (((grp) & 0x07) << 3))
+#define CYAPA_SMBUS_GROUP_DATA SMBUS_GROUP_CMD(REG_GROUP_DATA)
+#define CYAPA_SMBUS_GROUP_CTRL SMBUS_GROUP_CMD(REG_GROUP_CTRL)
+#define CYAPA_SMBUS_GROUP_CMD SMBUS_GROUP_CMD(REG_GROUP_CMD)
+#define CYAPA_SMBUS_GROUP_QUERY SMBUS_GROUP_CMD(REG_GROUP_QUERY)
+
+ /* for register block read/write command */
+#define CMD_BL_STATUS 0
+#define CMD_BL_HEAD 1
+#define CMD_BL_CMD 2
+#define CMD_BL_DATA 3
+#define CMD_BL_ALL 4
+#define CMD_BLK_PRODUCT_ID 5
+#define CMD_BLK_HEAD 6
+#define SMBUS_BLOCK_CMD(cmd) (0xC0 | (((cmd) & 0x1F) << 1))
+/* register block read/write command in bootloader mode */
+#define CYAPA_SMBUS_BL_STATUS SMBUS_BLOCK_CMD(CMD_BL_STATUS)
+#define CYAPA_SMBUS_BL_HEAD SMBUS_BLOCK_CMD(CMD_BL_HEAD)
+#define CYAPA_SMBUS_BL_CMD SMBUS_BLOCK_CMD(CMD_BL_CMD)
+#define CYAPA_SMBUS_BL_DATA SMBUS_BLOCK_CMD(CMD_BL_DATA)
+#define CYAPA_SMBUS_BL_ALL SMBUS_BLOCK_CMD(CMD_BL_ALL)
+/* register block read/write command in operational mode */
+#define CYAPA_SMBUS_BLK_PRODUCT_ID SMBUS_BLOCK_CMD(CMD_BLK_PRODUCT_ID)
+#define CYAPA_SMBUS_BLK_HEAD SMBUS_BLOCK_CMD(CMD_BLK_HEAD)
+
 static const struct cyapa_cmd_len cyapa_i2c_cmds[] = {
 	{CYAPA_OFFSET_SOFT_RESET, 1},
 	{REG_OFFSET_COMMAND_BASE + 1, 1},
@@ -302,6 +362,23 @@ static const struct cyapa_cmd_len cyapa_i2c_cmds[] = {
 	{BL_HEAD_CMD_STATUS_OFFSET, 32},
 	{REG_OFFSET_QUERY_BASE, PRODUCT_ID_SIZE},
 	{REG_OFFSET_DATA_BASE, 32}
+};
+
+static const struct cyapa_cmd_len cyapa_smbus_cmds[] = {
+	{CYAPA_SMBUS_RESET, 1},
+	{CYAPA_SMBUS_POWER_MODE, 1},
+	{CYAPA_SMBUS_DEV_STATUS, 1},
+	{CYAPA_SMBUS_GROUP_DATA, sizeof(struct cyapa_reg_data)},
+	{CYAPA_SMBUS_GROUP_CTRL, 0},
+	{CYAPA_SMBUS_GROUP_CMD, 2},
+	{CYAPA_SMBUS_GROUP_QUERY, QUERY_DATA_SIZE},
+	{CYAPA_SMBUS_BL_STATUS, 3},
+	{CYAPA_SMBUS_BL_HEAD, 16},
+	{CYAPA_SMBUS_BL_CMD, 16},
+	{CYAPA_SMBUS_BL_DATA, 16},
+	{CYAPA_SMBUS_BL_ALL, 32},
+	{CYAPA_SMBUS_BLK_PRODUCT_ID, PRODUCT_ID_SIZE},
+	{CYAPA_SMBUS_BLK_HEAD, 16},
 };
 
 #define BYTE_PER_LINE  8
@@ -373,26 +450,9 @@ static void cyapa_bl_disable_irq(struct cyapa *cyapa)
 	spin_unlock_irqrestore(&cyapa->miscdev_spinlock, flags);
 }
 
-static s32 cyapa_reg_read_byte(struct cyapa *cyapa, u8 reg)
-{
-	return i2c_smbus_read_byte_data(cyapa->client, reg);
-}
-
 /*
- * cyapa_reg_write_byte - write one byte to i2c register map.
- * @cyapa - private data structure of the trackpad driver.
- * @reg - the offset value of the i2c register map from offset 0.
- * @val - the value should be written to the register map.
- *
- * This function returns negative errno, else zero on success.
- */
-static s32 cyapa_reg_write_byte(struct cyapa *cyapa, u8 reg, u8 val)
-{
-	return i2c_smbus_write_byte_data(cyapa->client, reg, val);
-}
-
-/*
- * cyapa_reg_read_block - read a block of data from trackpad registers.
+ * cyapa_i2c_reg_read_block - read a block of data from trackpad registers
+ *      using i2c protocol.
  * @cyapa - private data structure of trackpad driver.
  * @reg - register at which to start reading.
  * @length - length of block to read, in bytes.
@@ -402,8 +462,8 @@ static s32 cyapa_reg_write_byte(struct cyapa *cyapa, u8 reg, u8 val)
  *
  * Note: The trackpad register block is 256 bytes.
  */
-static ssize_t cyapa_reg_read_block(struct cyapa *cyapa, u8 reg, size_t len,
-				    u8 *values)
+static ssize_t cyapa_i2c_reg_read_block(struct cyapa *cyapa, u8 reg, size_t len,
+					u8 *values)
 {
 	struct device *dev = &cyapa->client->dev;
 	ssize_t ret;
@@ -418,7 +478,8 @@ static ssize_t cyapa_reg_read_block(struct cyapa *cyapa, u8 reg, size_t len,
 }
 
 /*
- * cyapa_reg_write_block - write a block of data to trackpad registers.
+ * cyapa_i2c_reg_write_block - write a block of data to trackpad registers
+ *      using i2c protocol.
  * @cyapa - private data structure of trackpad driver.
  * @reg - register at which to start writing.
  * @length - length of block to write, in bytes.
@@ -428,8 +489,8 @@ static ssize_t cyapa_reg_read_block(struct cyapa *cyapa, u8 reg, size_t len,
  *
  * Note: The trackpad register block is 256 bytes.
  */
-static ssize_t cyapa_reg_write_block(struct cyapa *cyapa, u8 reg,
-				     size_t len, const u8 *values)
+static ssize_t cyapa_i2c_reg_write_block(struct cyapa *cyapa, u8 reg,
+					 size_t len, const u8 *values)
 {
 	struct device *dev = &cyapa->client->dev;
 	ssize_t ret;
@@ -441,18 +502,137 @@ static ssize_t cyapa_reg_write_block(struct cyapa *cyapa, u8 reg,
 	return (ret == 0) ? len : ret;
 }
 
+/*
+ * cyapa_smbus_reg_read_block - read a block data from trackpad
+ *      i2c register map.
+ * @cyapa  - private data structure of the trackpad driver.
+ * @cmd    - the properly encoded smbus command.
+ * @length - length of the block to be read in bytes.
+ * @values - pointer to the buffer that used to store register block
+ *           values read.
+ *
+ * Returns negative errno, else the number of bytes written.
+ *
+ * Note:
+ * In trackpad device, the memory block allocated for I2C register map
+ * is 256 bytes, so the max read block for I2C bus is 256 bytes.
+ */
+static ssize_t cyapa_smbus_reg_read_block(struct cyapa *cyapa, u8 cmd,
+					  size_t len, u8 *values)
+{
+	ssize_t ret;
+	u8 index;
+	u8 smbus_cmd;
+	u8 *buf;
+	struct i2c_client *client = cyapa->client;
+	struct device *dev = &client->dev;
+
+	if (!(SMBUS_BYTE_BLOCK_CMD_MASK & cmd))
+		return -EINVAL;
+
+	if (SMBUS_GROUP_BLOCK_CMD_MASK & cmd) {
+		/* read specific block registers command. */
+		smbus_cmd = SMBUS_ENCODE_RW(cmd, SMBUS_READ);
+		ret = i2c_smbus_read_block_data(client, smbus_cmd, values);
+		goto out;
+	}
+
+	for (index = 0; index * I2C_SMBUS_BLOCK_MAX < len; index++) {
+		smbus_cmd = SMBUS_ENCODE_IDX(cmd, index);
+		smbus_cmd = SMBUS_ENCODE_RW(smbus_cmd, SMBUS_READ);
+		buf = values + I2C_SMBUS_BLOCK_MAX * index;
+		ret = i2c_smbus_read_block_data(client, smbus_cmd, buf);
+		if (ret < 0)
+			goto out;
+	}
+
+out:
+	if (ret > 0) {
+		dev_dbg(dev, "read block cmd: 0x%02x length: %d\n",
+			smbus_cmd, len);
+		cyapa_dump_data(cyapa, len, values);
+	}
+	return (ret > 0) ? len : ret;
+}
+
+/*
+ * cyapa_smbus_reg_write_block - write a block data to trackpad
+ *      i2c register map.
+ * @cyapa  - private data structure of the trackpad driver.
+ * @cmd    - the properly encoded smbus command.
+ * @length - length of the block to be written in bytes.
+ * @values - pointer to the block data buffur that will be written.
+ *
+ * Returns negative errno, else the number of bytes written.
+ *
+ * Note:
+ * In trackpad device, the memory block allocated for I2C register map
+ * is 256 bytes, so the max write block for I2C bus is 256 bytes.
+ */
+static ssize_t cyapa_smbus_reg_write_block(struct cyapa *cyapa, u8 cmd,
+					   size_t len, u8 *values)
+{
+	ssize_t ret;
+	u8 length;
+	u8 index;
+	u8 smbus_cmd;
+	u8 *buf;
+	struct i2c_client *client = cyapa->client;
+	struct device *dev = &client->dev;
+
+	if (!(SMBUS_BYTE_BLOCK_CMD_MASK & cmd))
+		return -EINVAL;
+
+	dev_dbg(dev, "write block cmd: 0x%02x length: %d\n", cmd, len);
+	cyapa_dump_data(cyapa, len, (void *)values);
+
+	if (SMBUS_GROUP_BLOCK_CMD_MASK & cmd) {
+		/* write specific block registers command. */
+		smbus_cmd = SMBUS_ENCODE_RW(cmd, SMBUS_WRITE);
+		ret = i2c_smbus_write_block_data(client, smbus_cmd, len,
+						 values);
+		goto out;
+	}
+
+	for (index = 0; index * I2C_SMBUS_BLOCK_MAX < len; index++) {
+		smbus_cmd = SMBUS_ENCODE_IDX(cmd, index);
+		smbus_cmd = SMBUS_ENCODE_RW(smbus_cmd, SMBUS_WRITE);
+		buf = values + I2C_SMBUS_BLOCK_MAX * index;
+		length = len - I2C_SMBUS_BLOCK_MAX * index;
+		length = (length > I2C_SMBUS_BLOCK_MAX) ? I2C_SMBUS_BLOCK_MAX
+							: length;
+		ret = i2c_smbus_write_block_data(client, smbus_cmd, length,
+						 buf);
+		if (ret < 0)
+			goto out;
+	}
+
+out:
+	return (ret == 0) ? len : ret;
+}
+
 static s32 cyapa_read_byte(struct cyapa *cyapa, u8 cmd_idx)
 {
 	u8 cmd;
-	cmd = cyapa_i2c_cmds[cmd_idx].cmd;
-	return cyapa_reg_read_byte(cyapa, cmd);
+	if (cyapa->smbus && cyapa->touchpad_protocol == CYTP_SMBUS) {
+		cmd = cyapa_smbus_cmds[cmd_idx].cmd;
+		cmd = SMBUS_ENCODE_RW(cmd, SMBUS_READ);
+	} else {
+		cmd = cyapa_i2c_cmds[cmd_idx].cmd;
+	}
+	return i2c_smbus_read_byte_data(cyapa->client, cmd);
 }
 
 static s32 cyapa_write_byte(struct cyapa *cyapa, u8 cmd_idx, u8 value)
 {
 	u8 cmd;
-	cmd = cyapa_i2c_cmds[cmd_idx].cmd;
-	return cyapa_reg_write_byte(cyapa, cmd, value);
+	if (cyapa->smbus && cyapa->touchpad_protocol == CYTP_SMBUS) {
+		cmd = cyapa_smbus_cmds[cmd_idx].cmd;
+		cmd = SMBUS_ENCODE_RW(cmd, SMBUS_WRITE);
+	} else {
+		cmd = cyapa_i2c_cmds[cmd_idx].cmd;
+	}
+	return i2c_smbus_write_byte_data(cyapa->client, cmd, value);
 }
 
 static ssize_t cyapa_read_block(struct cyapa *cyapa, u8 cmd_idx, u8 *values)
@@ -460,10 +640,15 @@ static ssize_t cyapa_read_block(struct cyapa *cyapa, u8 cmd_idx, u8 *values)
 	u8 cmd;
 	size_t len;
 
-	cmd = cyapa_i2c_cmds[cmd_idx].cmd;
-	len = cyapa_i2c_cmds[cmd_idx].len;
-
-	return cyapa_reg_read_block(cyapa, cmd, len, values);
+	if (cyapa->smbus && cyapa->touchpad_protocol == CYTP_SMBUS) {
+		cmd = cyapa_smbus_cmds[cmd_idx].cmd;
+		len = cyapa_smbus_cmds[cmd_idx].len;
+		return cyapa_smbus_reg_read_block(cyapa, cmd, len, values);
+	} else {
+		cmd = cyapa_i2c_cmds[cmd_idx].cmd;
+		len = cyapa_i2c_cmds[cmd_idx].len;
+		return cyapa_i2c_reg_read_block(cyapa, cmd, len, values);
+	}
 }
 
 static ssize_t cyapa_write_block(struct cyapa *cyapa, u8 cmd_idx, u8 *values)
@@ -471,10 +656,15 @@ static ssize_t cyapa_write_block(struct cyapa *cyapa, u8 cmd_idx, u8 *values)
 	u8 cmd;
 	size_t len;
 
-	cmd = cyapa_i2c_cmds[cmd_idx].cmd;
-	len = cyapa_i2c_cmds[cmd_idx].len;
-
-	return cyapa_reg_write_block(cyapa, cmd, len, values);
+	if (cyapa->smbus && cyapa->touchpad_protocol == CYTP_SMBUS) {
+		cmd = cyapa_smbus_cmds[cmd_idx].cmd;
+		len = cyapa_smbus_cmds[cmd_idx].len;
+		return cyapa_smbus_reg_write_block(cyapa, cmd, len, values);
+	} else {
+		cmd = cyapa_i2c_cmds[cmd_idx].cmd;
+		len = cyapa_i2c_cmds[cmd_idx].len;
+		return cyapa_i2c_reg_write_block(cyapa, cmd, len, values);
+	}
 }
 
 
@@ -607,7 +797,8 @@ static ssize_t cyapa_misc_read(struct file *file, char __user *usr_buf,
 	if (ret < 0)
 		return ret;
 
-	ret = cyapa_reg_read_block(cyapa, (u16)reg_offset, reg_len, reg_buf);
+	ret = cyapa_i2c_reg_read_block(cyapa, (u16)reg_offset, reg_len,
+				       reg_buf);
 	if (ret < 0) {
 		dev_err(dev, "I2C read FAILED.\n");
 		return ret;
@@ -643,7 +834,7 @@ static ssize_t cyapa_misc_write(struct file *file, const char __user *usr_buf,
 	if (copy_from_user(reg_buf, usr_buf, (int)count))
 		return -EINVAL;
 
-	ret = cyapa_reg_write_block(cyapa,
+	ret = cyapa_i2c_reg_write_block(cyapa,
 					(u16)reg_offset,
 					(int)count,
 					reg_buf);
@@ -677,7 +868,12 @@ int cyapa_get_trackpad_run_mode(struct cyapa *cyapa,
 			/*
 			 * maybe firmware is switching its states,
 			 * wait for a moment.
+			 * if this is an smbus adapter, try the other protocol.
 			 */
+			if (cyapa->smbus) {
+				cyapa->touchpad_protocol =
+					!cyapa->touchpad_protocol;
+			}
 			msleep(300);
 			continue;
 		}
@@ -746,7 +942,7 @@ static int cyapa_send_mode_switch_cmd(struct cyapa *cyapa,
 	case CYAPA_CMD_IDLE_TO_ACTIVE:
 		cyapa_bl_disable_irq(cyapa);
 		/* send switch to active command. */
-		ret = cyapa_reg_write_block(cyapa, 0,
+		ret = cyapa_i2c_reg_write_block(cyapa, 0,
 				sizeof(bl_switch_active), bl_switch_active);
 		if (ret != sizeof(bl_switch_active)) {
 			dev_err(dev, "idle to active cmd failed, %d\n", ret);
@@ -757,7 +953,7 @@ static int cyapa_send_mode_switch_cmd(struct cyapa *cyapa,
 	case CYAPA_CMD_ACTIVE_TO_IDLE:
 		cyapa_bl_disable_irq(cyapa);
 		/* send switch to idle command.*/
-		ret = cyapa_reg_write_block(cyapa, 0,
+		ret = cyapa_i2c_reg_write_block(cyapa, 0,
 				sizeof(bl_switch_idle), bl_switch_idle);
 		if (ret != sizeof(bl_switch_idle)) {
 			dev_err(dev, "active to idle cmd failed, %d\n", ret);
@@ -767,7 +963,7 @@ static int cyapa_send_mode_switch_cmd(struct cyapa *cyapa,
 
 	case CYAPA_CMD_IDLE_TO_APP:
 		/* send command switch operational mode.*/
-		ret = cyapa_reg_write_block(cyapa, 0,
+		ret = cyapa_i2c_reg_write_block(cyapa, 0,
 				sizeof(bl_app_launch), bl_app_launch);
 		if (ret != sizeof(bl_app_launch)) {
 			dev_err(dev, "idle to app cmd failed, %d\n", ret);
@@ -797,6 +993,8 @@ static int cyapa_send_mode_switch_cmd(struct cyapa *cyapa,
 		/* update firmware working mode state in driver. */
 		spin_lock_irqsave(&cyapa->miscdev_spinlock, flags);
 		cyapa->in_bootloader = false;
+		if (cyapa->smbus)
+			cyapa->touchpad_protocol = CYTP_SMBUS;
 		spin_unlock_irqrestore(&cyapa->miscdev_spinlock, flags);
 
 		/* reconfig and update firmware information. */
@@ -815,6 +1013,7 @@ static int cyapa_send_mode_switch_cmd(struct cyapa *cyapa,
 	if (run_mode->rev_cmd != CYAPA_CMD_IDLE_TO_APP) {
 		spin_lock_irqsave(&cyapa->miscdev_spinlock, flags);
 		cyapa->in_bootloader = true;
+		cyapa->touchpad_protocol = CYTP_I2C;
 		spin_unlock_irqrestore(&cyapa->miscdev_spinlock, flags);
 	}
 
@@ -1369,6 +1568,19 @@ static void cyapa_close(struct input_dev *input)
 	cancel_delayed_work_sync(&cyapa->dwork);
 }
 
+static u8 cyapa_check_adapter_functionality(struct i2c_client *client)
+{
+	u8 ret = CYAPA_ADAPTER_FUNC_NONE;
+
+	if (i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
+		ret |= CYAPA_ADAPTER_FUNC_I2C;
+	if (i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA |
+				     I2C_FUNC_SMBUS_BLOCK_DATA |
+				     I2C_FUNC_SMBUS_I2C_BLOCK))
+		ret |= CYAPA_ADAPTER_FUNC_SMBUS;
+	return ret;
+}
+
 static int cyapa_create_input_dev(struct cyapa *cyapa)
 {
 	struct device *dev = &cyapa->client->dev;
@@ -1466,7 +1678,7 @@ static int cyapa_check_exit_bootloader(struct cyapa *cyapa)
 			(run_mode.bootloader_state ==
 				CYAPA_BOOTLOADER_ACTIVE_STATE)) {
 			/* bootloader active state. */
-			ret = cyapa_reg_write_block(cyapa, 0,
+			ret = cyapa_i2c_reg_write_block(cyapa, 0,
 				sizeof(bl_switch_idle), bl_switch_idle);
 
 			if (ret != sizeof(bl_switch_idle))
@@ -1481,14 +1693,15 @@ static int cyapa_check_exit_bootloader(struct cyapa *cyapa)
 			(run_mode.bootloader_state ==
 				CYAPA_BOOTLOADER_IDLE_STATE)) {
 			/* send command switch to operational mode. */
-			ret = cyapa_reg_write_block(cyapa, 0,
+			ret = cyapa_i2c_reg_write_block(cyapa, 0,
 				sizeof(bl_app_launch), bl_app_launch);
-
 			if (ret != sizeof(bl_app_launch))
 				continue;
 
 			/* wait firmware ready. */
 			msleep(300);
+			if (cyapa->smbus)
+				cyapa->touchpad_protocol = CYTP_SMBUS;
 			continue;
 		}
 	} while (tries--);
@@ -1505,12 +1718,11 @@ static int cyapa_set_power_mode(struct cyapa *cyapa, u8 power_mode)
 	u8 power;
 	int tries = 3;
 
-	power = cyapa_reg_read_byte(cyapa, REG_OFFSET_POWER_MODE);
+	power = cyapa_read_byte(cyapa, CYAPA_CMD_POWER_MODE);
 	power &= ~OP_POWER_MODE_MASK;
 	power |= ((power_mode << OP_POWER_MODE_SHIFT) & OP_POWER_MODE_MASK);
 	do {
-		ret = cyapa_reg_write_byte(cyapa,
-				REG_OFFSET_POWER_MODE, power);
+		ret = cyapa_write_byte(cyapa, CYAPA_CMD_POWER_MODE, power);
 		/* sleep at least 10 ms. */
 		usleep_range(SET_POWER_MODE_DELAY, 2 * SET_POWER_MODE_DELAY);
 	} while ((ret != 0) && (tries-- > 0));
@@ -1661,11 +1873,15 @@ static int __devinit cyapa_probe(struct i2c_client *client,
 				 const struct i2c_device_id *dev_id)
 {
 	int ret;
+	u8 adapter_func;
 	struct cyapa *cyapa;
 	struct device *dev = &client->dev;
 
-	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
+	adapter_func = cyapa_check_adapter_functionality(client);
+	if (adapter_func == CYAPA_ADAPTER_FUNC_NONE) {
+		dev_err(dev, "not a supported I2C/SMBus adapter\n");
 		return -EIO;
+	}
 
 	cyapa = kzalloc(sizeof(struct cyapa), GFP_KERNEL);
 	if (!cyapa) {
@@ -1676,6 +1892,10 @@ static int __devinit cyapa_probe(struct i2c_client *client,
 	cyapa->gen = CYAPA_GEN3;
 	cyapa->client = client;
 	global_cyapa = cyapa;
+	cyapa->adapter_func = adapter_func;
+	/* i2c isn't supported, set smbus */
+	if (cyapa->adapter_func == CYAPA_ADAPTER_FUNC_SMBUS)
+		cyapa->smbus = true;
 	cyapa->in_bootloader = true;
 	cyapa->misc_open_count = 0;
 	spin_lock_init(&cyapa->miscdev_spinlock);
