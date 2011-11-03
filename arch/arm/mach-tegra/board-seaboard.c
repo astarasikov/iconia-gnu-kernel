cf2934e693072df9668b0795be12d031b6472b3a
Daniel Kurtz <djkurtz@chromium.org>
2011-11-03 11:25:06 +0800

    CHROMIUM: ARM: tegra: seaboard: cyapa: remove platform_data
    irq_gpio is already specified when instantiating i2c_board_info
    (see board-seaboard.c).
    
    geni is replaced with a state variable initialized to GEN3 and adjusted
    at runtime.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=chromium-os:21047
    TEST=touchpad still works (cyapa irq is still configured properly)
    
    Change-Id: I24650a74ed8aa62ce74cc024326f3528111df896
    Reviewed-on: https://gerrit.chromium.org/gerrit/11110
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Reviewed-by: Dudley Du <dudl@cypress.com>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -153,11 +153,6 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	},
 };
 
-static struct cyapa_platform_data cyapa_i2c_platform_data = {
-	.gen				= CYAPA_GEN3,
-	.irq_gpio			= TEGRA_GPIO_CYTP_INT,
-};
-
 static struct tegra_i2c_platform_data seaboard_i2c1_platform_data = {
         .adapter_nr     = 0,
         .bus_count      = 1,
@@ -636,7 +631,6 @@ static struct i2c_board_info __initdata ak8975_device = {
 static struct i2c_board_info __initdata cyapa_device = {
 	I2C_BOARD_INFO(CYAPA_I2C_NAME, 0x67),
 	.irq		= TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_CYTP_INT),
-	.platform_data	= &cyapa_i2c_platform_data,
 };
 
 static struct i2c_board_info __initdata mpu3050_device = {
